b34fc29b5a216dd4ae176e65f8cd730c4916809f
PJDong <1115957667@qq.com>
2020-12-11 16:09:19 +0800

    Update LabelSmoothing.py

--- project/utils/LabelSmoothing.py
+++ project/utils/LabelSmoothing.py
@@ -10,23 +10,23 @@ class LSR(nn.Module):
         self.log_softmax = nn.LogSoftmax(dim=1)
         self.e = e
         self.reduction = reduction
-    
+
     def _one_hot(self, labels, classes, value=1):
         """
             Convert labels to one hot vectors
-        
+
         Args:
             labels: torch tensor in format [label1, label2, label3, ...]
             classes: int, number of classes
             value: label value in one hot vector, default to 1
-        
+
         Returns:
             return one hot format labels in shape [batchsize, classes]
         """
 
         one_hot = torch.zeros(labels.size(0), classes)
 
-        #labels and value_added  size must match
+        # labels and value_added  size must match
         labels = labels.view(labels.size(0), -1)
         value_added = torch.Tensor(labels.size(0), 1).fill_(value)
 
@@ -44,7 +44,7 @@ class LSR(nn.Module):
             target: target in form with [label1, label2, label_batchsize]
             length: length of one-hot format(number of classes)
             smooth_factor: smooth factor for label smooth
-        
+
         Returns:
             smoothed labels in one hot format
         """
@@ -57,16 +57,15 @@ class LSR(nn.Module):
 
         if x.size(0) != target.size(0):
             raise ValueError('Expected input batchsize ({}) to match target batch_size({})'
-                    .format(x.size(0), target.size(0)))
+                             .format(x.size(0), target.size(0)))
 
         if x.dim() < 2:
             raise ValueError('Expected input tensor to have least 2 dimensions(got {})'
-                    .format(x.size(0)))
+                             .format(x.size(0)))
 
         if x.dim() != 2:
             raise ValueError('Only 2 dimension tensor are implemented, (got {})'
-                    .format(x.size()))
-
+                             .format(x.size()))
 
         smoothed_target = self._smooth_label(target, x.size(1), self.e)
         x = self.log_softmax(x)
@@ -74,12 +73,33 @@ class LSR(nn.Module):
 
         if self.reduction == 'none':
             return loss
-        
+
         elif self.reduction == 'sum':
             return torch.sum(loss)
-        
+
         elif self.reduction == 'mean':
             return torch.mean(loss)
-        
+
         else:
-            raise ValueError('unrecognized option, expect reduction to be one of none, mean, sum')
+            raise ValueError(
+                'unrecognized option, expect reduction to be one of none, mean, sum')
+
+
+class LabelSmoothLoss(nn.Module):
+    # anthor implementation
+    def __init__(self, class_num, smooth=0.0):
+        super(LabelSmoothLoss, self).__init__()
+        self.class_num = class_num
+        self.smooth = smooth
+        self.loss = nn.KLDivLoss()
+        self.true_dist = None
+
+    def forward(self, X, target):
+        assert X.size(1) == self.class_num
+        true_dist = X.clone()
+        true_dist.fill_(self.smooth / (self.class_num - 1))
+        true_dist.scatter_(1, target.data.unsqueeze(1), 1.0 - self.smooth)
+        true_dist = true_dist.detach()
+        self.true_dist = true_dist
+
+        return self.loss(X, true_dist)
