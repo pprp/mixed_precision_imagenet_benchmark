bcecca3b0167f0b4f5d906db04d32f6d07438356
PJDong <1115957667@qq.com>
2020-12-12 09:55:56 +0800

    update

---
+++ project/utils/OtherTransform.py
@@ -0,0 +1,292 @@
+class RandomErasing(object):
+    def __init__(self, EPSILON=0.5, sl=0.02, sh=0.4, r1=0.3, mean=[0.4914, 0.4822, 0.4465]):
+        self.EPSILON = EPSILON
+        self.mean = mean
+        self.sl = sl
+        self.sh = sh
+        self.r1 = r1
+
+    def __call__(self, img):
+
+        if random.uniform(0, 1) > self.EPSILON:
+            return img
+
+        for attempt in range(100):
+            area = img.size()[1] * img.size()[2]
+
+            target_area = random.uniform(self.sl, self.sh) * area
+            aspect_ratio = random.uniform(self.r1, 1/self.r1)
+
+            h = int(round(math.sqrt(target_area * aspect_ratio)))
+            w = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if w <= img.size()[2] and h <= img.size()[1]:
+                x1 = random.randint(0, img.size()[1] - h)
+                y1 = random.randint(0, img.size()[2] - w)
+                if img.size()[0] == 3:
+                    #img[0, x1:x1+h, y1:y1+w] = random.uniform(0, 1)
+                    #img[1, x1:x1+h, y1:y1+w] = random.uniform(0, 1)
+                    #img[2, x1:x1+h, y1:y1+w] = random.uniform(0, 1)
+                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
+                    img[1, x1:x1+h, y1:y1+w] = self.mean[1]
+                    img[2, x1:x1+h, y1:y1+w] = self.mean[2]
+                    #img[:, x1:x1+h, y1:y1+w] = torch.from_numpy(np.random.rand(3, h, w))
+                else:
+                    img[0, x1:x1+h, y1:y1+w] = self.mean[1]
+                    # img[0, x1:x1+h, y1:y1+w] = torch.from_numpy(np.random.rand(1, h, w))
+                return img
+
+        return img
+
+
+def random_crop(img, boxes):
+    '''Crop the given PIL image to a random size and aspect ratio.
+    A crop of random size of (0.08 to 1.0) of the original size and a random
+    aspect ratio of 3/4 to 4/3 of the original aspect ratio is made.
+    Args:
+      img: (PIL.Image) image to be cropped.
+      boxes: (tensor) object boxes, sized [#ojb,4].
+    Returns:
+      img: (PIL.Image) randomly cropped image.
+      boxes: (tensor) randomly cropped boxes.
+    '''
+    success = False
+    for attempt in range(10):
+        area = img.size[0] * img.size[1]
+        target_area = random.uniform(0.56, 1.0) * area
+        aspect_ratio = random.uniform(3. / 4, 4. / 3)
+
+        w = int(round(math.sqrt(target_area * aspect_ratio)))
+        h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+        if random.random() < 0.5:
+            w, h = h, w
+
+        if w <= img.size[0] and h <= img.size[1]:
+            x = random.randint(0, img.size[0] - w)
+            y = random.randint(0, img.size[1] - h)
+            success = True
+            break
+
+    # Fallback
+    if not success:
+        w = h = min(img.size[0], img.size[1])
+        x = (img.size[0] - w) // 2
+        y = (img.size[1] - h) // 2
+
+    img = img.crop((x, y, x+w, y+h))
+    boxes -= torch.Tensor([x, y, x, y])
+    boxes[:, 0::2].clamp_(min=0, max=w-1)
+    boxes[:, 1::2].clamp_(min=0, max=h-1)
+    return img, boxes
+
+
+class Grayscale(object):
+    def __call__(self, img):
+        gs = img.clone()
+        gs[0].mul_(0.299).add_(0.587, gs[1]).add_(0.114, gs[2])
+        gs[1].copy_(gs[0])
+        gs[2].copy_(gs[0])
+        return gs
+
+
+class Saturation(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        gs = Grayscale()(img)
+        alpha = random.uniform(0, self.var)
+        return img.lerp(gs, alpha)
+
+
+class Brightness(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        gs = img.new().resize_as_(img).zero_()
+        alpha = random.uniform(0, self.var)
+        return img.lerp(gs, alpha)
+
+
+class Contrast(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        gs = Grayscale()(img)
+        gs.fill_(gs.mean())
+        alpha = random.uniform(0, self.var)
+        return img.lerp(gs, alpha)
+
+
+class RandomOrder(object):
+    """ Composes several transforms together in random order.
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img):
+        if self.transforms is None:
+            return img
+        order = torch.randperm(len(self.transforms))
+        for i in order:
+            img = self.transforms[i](img)
+        return img
+
+
+# from fastai solution
+# not avaliable now
+
+
+class RectangularCropTfm(object):
+    def __init__(self, idx2ar, target_size):
+        self.idx2ar, self.target_size = idx2ar, target_size
+
+    def __call__(self, img, idx):
+        target_ar = self.idx2ar[idx]
+        if target_ar < 1:
+            w = int(self.target_size/target_ar)
+            size = (w//8*8, self.target_size)
+        else:
+            h = int(self.target_size*target_ar)
+            size = (self.target_size, h//8*8)
+        return transforms.functional.center_crop(img, size)
+
+# Step 1: sort images by aspect ratio
+
+
+# def sort_ar(data, valdir):
+#     idx2ar_file = data/'sorted_idxar.p'
+#     if os.path.isfile(idx2ar_file):
+#         return pickle.load(open(idx2ar_file, 'rb'))
+#     print('Creating AR indexes. Please be patient this may take a couple minutes...')
+#     val_dataset = torchvision.datasets.ImageFolder(valdir)
+#     sizes = [img[0].size for img in tqdm(val_dataset, total=len(val_dataset))]
+#     idx_ar = [(i, round(s[0]/s[1], 5)) for i, s in enumerate(sizes)]
+#     sorted_idxar = sorted(idx_ar, key=lambda x: x[1])
+#     pickle.dump(sorted_idxar, open(idx2ar_file, 'wb'))
+#     return sorted_idxar
+
+# Step 2: chunk images by batch size. This way we can crop each image to the batch aspect ratio mean
+
+
+# def chunks(l, n):
+#     n = max(1, n)
+#     return (l[i:i+n] for i in range(0, len(l), n))
+
+# Step 3: map image index to batch aspect ratio mean so our transform function knows where to crop
+
+
+# def map_idx2ar(idx_ar_sorted, batch_size):
+#     ar_chunks = list(chunks(idx_ar_sorted, batch_size))
+#     idx2ar = {}
+#     ar_means = []
+#     for chunk in ar_chunks:
+#         idxs, ars = list(zip(*chunk))
+#         mean = round(np.mean(ars), 5)
+#         ar_means.append(mean)
+#         for idx in idxs:
+#             idx2ar[idx] = mean
+#     return idx2ar, ar_means
+
+
+class RandomFlip(object):
+    """Randomly flips the given PIL.Image with a probability of 0.25 horizontal,
+                                                                0.25 vertical,
+                                                                0.5 as is
+    """
+
+    def __call__(self, img):
+        dispatcher = {
+            0: img,
+            1: img,
+            2: img.transpose(im.FLIP_LEFT_RIGHT),
+            3: img.transpose(im.FLIP_TOP_BOTTOM)
+        }
+
+        return dispatcher[random.randint(0, 3)]  # randint is inclusive
+
+
+class RandomRotate(object):
+    """Randomly rotate the given PIL.Image with a probability of 1/6 90°,
+                                                                 1/6 180°,
+                                                                 1/6 270°,
+                                                                 1/2 as is
+    """
+
+    def __call__(self, img):
+        dispatcher = {
+            0: img,
+            1: img,
+            2: img,
+            3: img.transpose(im.ROTATE_90),
+            4: img.transpose(im.ROTATE_180),
+            5: img.transpose(im.ROTATE_270)
+        }
+
+        return dispatcher[random.randint(0, 5)]  # randint is inclusive
+
+
+class PILColorBalance(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        alpha = random.uniform(1 - self.var, 1 + self.var)
+        return ie.Color(img).enhance(alpha)
+
+
+class PILContrast(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        alpha = random.uniform(1 - self.var, 1 + self.var)
+        return ie.Contrast(img).enhance(alpha)
+
+
+class PILBrightness(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        alpha = random.uniform(1 - self.var, 1 + self.var)
+        return ie.Brightness(img).enhance(alpha)
+
+
+class PILSharpness(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        alpha = random.uniform(1 - self.var, 1 + self.var)
+        return ie.Sharpness(img).enhance(alpha)
+
+
+# Check ImageEnhancer effect: https://www.youtube.com/watch?v=_7iDTpTop04
+# Not documented but all enhancements can go beyond 1.0 to 2
+# Image must be RGB
+# Use Pillow-SIMD because Pillow is too slow
+class PowerPIL(RandomOrder):
+    def __init__(self, rotate=True,
+                 flip=True,
+                 colorbalance=0.4,
+                 contrast=0.4,
+                 brightness=0.4,
+                 sharpness=0.4):
+        self.transforms = []
+        if rotate:
+            self.transforms.append(RandomRotate())
+        if flip:
+            self.transforms.append(RandomFlip())
+        if brightness != 0:
+            self.transforms.append(PILBrightness(brightness))
+        if contrast != 0:
+            self.transforms.append(PILContrast(contrast))
+        if colorbalance != 0:
+            self.transforms.append(PILColorBalance(colorbalance))
+        if sharpness != 0:
+            self.transforms.append(PILSharpness(sharpness))
